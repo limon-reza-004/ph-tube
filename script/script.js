e11e50bd28430a6c9819a8684b018f0e1b5f4f5c
limonreza <limon4reza000@gmail.com>
2026-04-19 04:45:36 +0600

    loaded video from api

--- script/script.js
+++ script/script.js
@@ -20,4 +20,69 @@ function displayCategories(categories) {
     }
 }
 
+
+
+
+
+
+
+// {
+//     "category_id": "1003",
+//     "video_id": "aaai",
+//     "thumbnail": "https://i.ibb.co/kc8CCFs/30-rock.png",
+//     "title": "30 Rock",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/YZN9rQZ/tina.jpg",
+//             "profile_name": "Tina Fey",
+//             "verified": false
+//         }
+//     ],
+//     "others": {
+//         "views": "4.5K",
+//         "posted_date": "14800"
+//     },
+//     "description": "'30 Rock,' led by Tina Fey, is a comedy series that has garnered 4.5K views. The show is a witty and humorous take on the behind-the-scenes antics of a fictional live comedy show. With its sharp writing and unforgettable characters, '30 Rock' is perfect for fans of smart, satirical humor and engaging storylines."
+// }
+
+
+
+
+
+// load videos
+function loadVideos() {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(response => response.json())
+        .then(data => displayVideos(data.videos))
+}
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("video-container");
+
+    videos.forEach((video) => {
+        // console.log(video)
+
+        const videoCard = document.createElement("div");
+
+        videoCard.innerHTML = `
+        <div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+  </div>
+    </div>
+
+        `;
+
+        videoContainer.append(videoCard);
+
+    });
+}
+
 loadCategories();
+loadVideos();
